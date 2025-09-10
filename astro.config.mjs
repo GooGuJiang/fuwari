9fde9c2b19e707d061a73c09c8d614d22369e19e
咕谷酱 <gu@gmoe.cc>
2025-09-10 10:55:56 +0800

    fix error

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-copy-button.js";
 
-import Compress from "@playform/compress/vite";
+import Compress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
